aad78ecad83d8669b644445f71d0da0840a14003
Wilkel Giovanni <wilkelgiovanni@gmail.com>
2020-04-16 12:10:57 -0400

    Props al componente Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import exerciseImg from '../images/exercise.png';
+// import exerciseImg from '../images/exercise.png';
 import circlesImg from '../images/circles.png';
 import './styles/Card.css';
 import 'bootstrap/dist/css/bootstrap.css';
 class Card extends Component {
+
     render() {
+        const { title, description, img, leftColor, rightColor} = this.props;
         return <div className="card mx-auto Fitness-Card"
             style={{
-                backgroundImage: `url(${circlesImg}), linear-gradient(to right, #A74CF2, #617BFB)`
+                backgroundImage: `url(${circlesImg}), linear-gradient(to right, ${leftColor}, ${rightColor})`
             }}
             >
             <div className="card-body">
                 <div className="row">
                     <div className="col-6">
-                        <img src={exerciseImg} className="float-right"/>
+                        <img src={img} className="float-right"/>
                     </div>
                     <div className="col-6 Fitness-Card-Info">
-                        <h1>Guías tecnicas</h1>
-                        <p>
-                            Aprende ejercicios callejeros y calistenia increíbles
-                        </p>
+                        <h1>{title}</h1>
+                        <p>{description}</p>
                     </div>
                 </div>
             </div>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from './components/Card.js';
 
-ReactDOM.render(<Card/>,
+ReactDOM.render(<Card
+                  title="Guías tecnicas"
+                  description="Aprende ejercicios callejeros y calistenia increíbles"
+                  img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
+                  leftColor="#A74CF2"
+                  rightColor="#617BFB"
+                />,
   document.getElementById('root')
 );
 
