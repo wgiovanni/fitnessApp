89b1cd51ff352501d1a7029946965b454a88032c
Wilkel Giovanni <wilkelgiovanni@gmail.com>
2020-04-16 17:41:13 -0400

    Levantando estado

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 class Card extends Component {
 
     render() {
-        const { title, description, img, leftColor, rightColor} = this.props;
+        const { title, description, img, leftColor, rightColor } = this.props;
         return <div className="card mx-auto Fitness-Card"
             style={{
                 backgroundImage: `url(${circlesImg}), linear-gradient(to right, ${leftColor}, ${rightColor})`
@@ -17,7 +17,7 @@ class Card extends Component {
             <div className="card-body">
                 <div className="row">
                     <div className="col-6">
-                        <img src={img} className="float-right" alt={title}/>
+                        <img src={img} className="float-right"/>
                     </div>
                     <div className="col-6 Fitness-Card-Info">
                         <h1>{title}</h1>
