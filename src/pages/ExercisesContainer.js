255717205b85c6aa766779e50673065d6092bb78
Wilkel Giovanni <wilkelgiovanni@gmail.com>
2020-04-17 19:25:15 -0400

    Hook y State Hook

--- src/pages/ExercisesContainer.js
+++ src/pages/ExercisesContainer.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import Welcome from '../components/Welcome.js';
 import ExercisesList from '../components/ExercisesList.js';
@@ -7,48 +7,62 @@ import Spinner from '../components/Spinner';
 import FatalError from './500';
 import Exercises from './Exercises.js';
 
+const ExercisesContainer = () => {
 
-class ExercisesContainer extends Component {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-    constructor(props)  {
-        super(props);
-        this.state = {
-            data: [],
-            loading: true,
-            error: null
-        }
-    }
+    if (loading)
+        return <Spinner/>
+    if (error)
+        return <FatalError/>
+    return <Exercises 
+                data={data}
+            />
 
-    async componentDidMount() {
-        await this.fetchExercises()
-    }
-
-    fetchExercises = async () => {
-        try {
-            let res = await fetch('http://localhost:8000/api/exercises')
-            let data = await res.json()
-    
-            this.setState({
-                data,
-                loading: false
-            })
-        } catch (error) {
-            this.setState({
-                loading: false,
-                error
-            })
-        }
-
-    }
-    render() {
-        if (this.state.loading)
-            return <Spinner/>
-        if (this.state.error)
-            return <FatalError/>
-        return <Exercises 
-                    data={this.state.data}
-                />
-    } 
 }
+// class ExercisesContainer extends Component {
+
+//     constructor(props)  {
+//         super(props);
+//         this.state = {
+//             data: [],
+//             loading: true,
+//             error: null
+//         }
+//     }
+
+//     async componentDidMount() {
+//         await this.fetchExercises()
+//     }
+
+//     fetchExercises = async () => {
+//         try {
+//             let res = await fetch('http://localhost:8000/api/exercises')
+//             let data = await res.json()
+    
+//             this.setState({
+//                 data,
+//                 loading: false
+//             })
+//         } catch (error) {
+//             this.setState({
+//                 loading: false,
+//                 error
+//             })
+//         }
+
+//     }
+//     render() {
+//         if (this.state.loading)
+//             return <Spinner/>
+//         if (this.state.error)
+//             return <FatalError/>
+//         return <Exercises 
+//                     data={this.state.data}
+//                 />
+//     } 
+// }
 
 export default ExercisesContainer;
