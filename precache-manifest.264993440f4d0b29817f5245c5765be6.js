5ac768f5040c76a042e0673838afab6cb4cf30a9
Wilkel Giovanni <wilkelgiovanni@gmail.com>
2020-05-14 13:32:49 -0400

    Updates

---
+++ precache-manifest.264993440f4d0b29817f5245c5765be6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "495ffd1f1c117d4d022751c198cca00b",
+    "url": "/fitnessApp/index.html"
+  },
+  {
+    "revision": "7a9d62df75df802be59e",
+    "url": "/fitnessApp/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "5ef76c064de22ab79344",
+    "url": "/fitnessApp/static/css/main.13c2edca.chunk.css"
+  },
+  {
+    "revision": "7a9d62df75df802be59e",
+    "url": "/fitnessApp/static/js/2.efdbe106.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fitnessApp/static/js/2.efdbe106.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ef76c064de22ab79344",
+    "url": "/fitnessApp/static/js/main.adc8ed62.chunk.js"
+  },
+  {
+    "revision": "37ead2a64e9471aa7007",
+    "url": "/fitnessApp/static/js/runtime-main.1c62fc79.js"
+  },
+  {
+    "revision": "f16a1ad5b4b9c86c0f30cd484308cb3b",
+    "url": "/fitnessApp/static/media/404.f16a1ad5.png"
+  },
+  {
+    "revision": "854842fa495e13de2d54968cec61b6d1",
+    "url": "/fitnessApp/static/media/500.854842fa.png"
+  },
+  {
+    "revision": "eb728687589f75caecdb7f2b3d71c1d5",
+    "url": "/fitnessApp/static/media/circles.eb728687.png"
+  }
+]);
